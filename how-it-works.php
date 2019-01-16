84393f0919eb3f0a7ea107b4479e1222c5919936
Graham Clements <graham.clements@student.curtin.edu.au>
2019-01-16 23:12:41 +0000

    latest updates

---
+++ how-it-works.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="/css/styles.css" rel="stylesheet" type="text/css">
+    <title>How it works page Toyota Mirai</title>
+  </head>
+  <body>
+    
+    <div class="container">
+   
+    <div class="row">
+      <div class="col-2">
+          <img src="images/toyota-logo.jpg" style="width: 60px;">
+      </div>
+      <div class="col-8">
+          <h1 class="text-center" id="red">Toyota Mirai</h1>   
+      </div>
+    
+   </div>
+</div>
+    
+<nav class="navbar navbar-expand-sm navbar-light bg-light" data-toggle="affix">
+    <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" data-target="#homepagetarget" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+          <div class="collapse navbar-collapse text-center" id="homepagetarget">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+            </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#">About the Mirai</a>
+            </li>
+          <li class="nav-item">
+          <a class="nav-link active" href="how-it-works">How it Works</a>
+            </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#">Fuel Stations</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">Reviews</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">Get a Mirai</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+             </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">Contact</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+          </li>  
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+
+<h2 class="text-center">How it Works</h2>
+
+ <div class="container col-12">
+    <div class="row">
+      <div class="col-4">
+          
+      </div>
+    
+    
+    
+    <div class="col-4"> 
+  <img class="img-fluid"src="images/mirai-chasis.png">
+  </div>
+  </row>
+</div>
+    
+ </body>
+
+</html>
--- index.php
+++ index.php
@@ -8,33 +8,116 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="/css/styles.css" rel="stylesheet" type="text/css">
-    <title>Homepage Toyota Miria</title>
+    <title>Homepage Toyota Mirai</title>
   </head>
   <body>
-    <nav class="navbar bg-light navbar-light navbar-expand-sm">
-      <div class="container">
-         <div class="navbar-brand d-none d-sm-inline-block">
-          <img src="images/toyota-logo.jpg" style="width: 50px;">
-        </div><div class="navbar-brand d-none d-sm-inline-block">Toyota Miria</div>
-       
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">Home</a>
-          <a class="nav-item nav-link" href="#">About the Miria</a>
-          <a class="nav-item nav-link" href="#">How it Works</a>
-          <a class="nav-item nav-link" href="#">Fuel Stations</a>
-          <a class="nav-item nav-link" href="#">Reviews</a>
-          <a class="nav-item nav-link" href="#">Get a Mirai</a>
-          <a class="nav-item nav-link" href="#">Blog</a>
-          <a class="nav-item nav-link" href="#">Contact</a>
-          <a class="nav-item nav-link" href="#">About Us</a>
-        </div>
+    
+    <div class="container">
+   
+    <div class="row">
+      <div class="col-2">
+          <img src="images/toyota-logo.jpg" style="width: 60px;">
       </div>
-    </nav>
+      <div class="col-8">
+          <h1 class="text-center" id="red">Toyota Mirai</h1>   
+      </div>
     
+   </div>
+</div>
     
-    
-    <h1></h1>
+<nav class="navbar navbar-expand-sm navbar-light bg-light" data-toggle="affix">
+    <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" data-target="#homepagetarget" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+          <div class="collapse navbar-collapse text-center" id="homepagetarget">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#">About the Mirai</a>
+            </li>
+          <li class="nav-item">
+          <a class="nav-link" href="how-it-works.php">How it Works</a>
+            </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#">Fuel Stations</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">Reviews</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">Get a Mirai</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+             </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">Contact</a>
+             </li>
+           <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+          </li>  
+            </ul>
+        </div>
+    </div>
+</nav>
+<main role="main" class="container">
 
+      <section class="col-sm-10 offset-1">
+          <div class="carousel" id="homepagecarousel" data-ride="carousel">
+              <div class="carousel-item">
+                <img class="img-fluid" src="images/carousel-mirai-fuelpumps.png" alt="maria at fuel pump">
+              </div>
+              <div class="carousel-item">
+                <img class="img-fluid" src="images/carousel-mirai-on-road.png" alt="maria on a road">
+              </div>
+              <div class="carousel-item active">
+                <img class="img-fluid" src="images/carousel-mirai.png" alt="maria">
+              </div>
+              <a class="carousel-control-prev" href="#homepagecarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true">
+                  <span class="sr-only">Previous</span>
+                </span>
+              </a>
+              <a class="carousel-control-next" href="#homepagecarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true">
+                  <span class="sr-only">Next</span>
+                </span>
+              </a>
+            </div> <!--end carousel div-->
+            </section>
+            
+            <div class="container">
+   
+   <div class="container col-12">
+    <div class="row">
+      <div class="col-3-md" id="homebox">
+          <h5 class="homeboxes text-center">Stylish</h5>
+          <img class="img-fluid" id="homeimage2" src="images/mirai-side.png">
+      </div>
+      <div class="col-3-md" id="homebox">
+           <h5 class="homeboxes text-center">Hydrogen Fuelled</h5>
+          <img class="img-fluid" id="homeimage" src="images/hydrogen-fuel.png">
+      </div>
+      <div class="col-3-md" id="homebox">
+          <h5 class="homeboxes text-center">Safe</h5>
+         <img class="img-fluid" id="homeimage2" src="images/mirai-rear.png">
+      </div> 
+     <div class="col-3-md" id="homebox">
+           <h5 class="homeboxes text-center">Environmentally Friendly</h5>
+          <img class="img-fluid" id="homeimage" src="images/mirai-mountains.png">
+          </div>
+     </div>
+    
+   </div>
+</div>
+    
+            
+           
+    <h1></h1>
+</main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
